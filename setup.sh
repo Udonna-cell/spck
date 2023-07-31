8d8aefc9b6bd1dd719d508a4527da8593cbec42d
udonna-cell <orabuezestanley@gmail.com>
2023-07-31 03:16:08 +0100

    fixed multiple check on a single command

--- setup.sh
+++ setup.sh
@@ -8,14 +8,13 @@ echo "setting spck cli"
 PACKAGE_NAME=("figlet") # write the names of commands you need to check for
 
 for package in "${PACKAGE_NAME[@]}"; do
-    while read -r INSTALLED_PACKAGE; do
-        # Check if the package is installed
-        if [[ $INSTALLED_PACKAGE -eq $package ]]; then
-            echo "$PACKAGE_NAME is installed."
-        else
-            echo "$PACKAGE_NAME is not installed."
-        fi
-    done < <(tr "/" " " <<<"$(pkg list-installed)" | cut -d " " -f 1)
+    INSTALLED_PACKAGE=$(tr "/" " " <<<"$(pkg list-installed)" | cut -d " " -f 1)
+    # Check if the package is installed
+    if [[ $INSTALLED_PACKAGE -eq $package ]]; then
+        echo "$PACKAGE_NAME is installed."
+    else
+        echo "$PACKAGE_NAME is not installed."
+    fi
 done
 
 if [ -d "repo" ]; then
