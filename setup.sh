52bda26a1a98396c9ab4275ab435aff2eefcd017
udonna-cell <orabuezestanley@gmail.com>
2023-07-31 13:08:08 +0100

    checking installation program is now complete and ready

--- setup.sh
+++ setup.sh
@@ -5,15 +5,15 @@ figlet "Spck cpanel"
 echo "setting spck cli"
 
 # Replace 'package_name' with the name of the package you want to check
-PACKAGE_NAME=("figlet" "tr" "stanley") # write the names of commands you need to check for
+PACKAGE_NAME=("figlet" "tr" "pv") # write the names of commands you need to check for
 
 for package in "${PACKAGE_NAME[@]}"; do
     INSTALLED_PACKAGE=$(tr "/" " " <<<"$(pkg list-installed)" | cut -d " " -f 1)
     # Check if the package is installed
-    if echo "$INSTALLED_PACKAGE" | grep -q "^$PACKAGE_NAME\$"; then
-        echo "$PACKAGE_NAME is installed."
+    if echo "$INSTALLED_PACKAGE" | grep -q "$package"; then
+        echo "$package is installed."
     else
-        echo "$PACKAGE_NAME is not installed."
+        echo "$package is not installed."
     fi
 done
 
