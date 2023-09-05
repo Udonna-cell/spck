99eb138cb8f740a6164ff0e2d9b0eb401153e4f4
udonna-cell <orabuezestanley@gmail.com>
2023-09-05 21:25:22 +0100

    still working on setup command

--- bin/setup.js
+++ bin/setup.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-
-import loader from "../lib/load"
+import chalk from 'chalk'
+import {loader}  from "../lib/load.js"
 
 let data = {
     shell: "mkdir .config"
 }
+console.log(chalk.gray('setting up spck-cli...'))
 loader(data)
