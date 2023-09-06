7c8204dce56a819f3ff84bd92a1ddea013c5a52d
udonna-cell <orabuezestanley@gmail.com>
2023-09-06 01:23:10 +0100

    loader is ready

--- bin/setup.js
+++ bin/setup.js
@@ -2,8 +2,21 @@
 import chalk from 'chalk'
 import {loader}  from "../lib/load.js"
 
-let data = {
-    shell: "mkdir .config"
-}
+let task = [
+    {
+        cli: "mkdir .config",
+        success: "config folder created",
+        err: "This base has spck cli setup already"
+    },
+    {
+        cli: "touch .config/spck.config.js",
+        success: "config file created",
+        err: "This base has spck cli setup already"
+    }
+]
+let option = {
+    text: "creating a config folder",
+    spinner: "bouncingBar"
+  }
 console.log(chalk.gray('setting up spck-cli...'))
-loader(data)
+loader(task,option)
